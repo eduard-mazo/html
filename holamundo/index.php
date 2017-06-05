c73e56dcb0084d78d75502c90155e573a98748e6
Eduard Mazo <eduard.mazo@gmail.com>
2017-06-04 22:44:01 -0500

    Add files via upload

---
+++ holamundo/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SLIDE OUT BAR</title>
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:200,800" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<i class="fa fa-bars toggle_menu"></i>
+	
+	<div class="sidebar_menu">
+		<i class="fa fa-times"></i>
+		<center>
+			<a href="index.php"><h1 class="boxed_item">EDUARD <span class="logo_bold">MAZO</span></h1></a>
+			<h2 class="logo_title">DROGUERIA LUNA</h2>
+		</center>
+		<ul class="navigation_selection">
+			<li class="navigation_item">PERFIL</li>
+			<li class="navigation_item">FACTURACION</li>
+			<li class="navigation_item">BALANCE</li>
+			<li class="navigation_item">FALTANTES</li>
+		</ul>
+		
+	</div>
+</body>
+</html>
